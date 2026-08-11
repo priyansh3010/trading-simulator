49c7d3e554cba5784977eab9e2ea0b4a1b02307f
priyansh3010 <pri.obhan30@gmail.com>
2026-08-11 22:14:03 +0530

    feat: added instrument ids, and a basic matching algorithm

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -5,23 +5,99 @@
 #include <list>
 #include <map>
 #include <unordered_map>
+#include <string>
 using namespace std;
 
 OrderBook::OrderBook() {
     orderId_ = 0;
     participantId_ = 0;
+    instrumentId_ = 0;
 }
 
-void OrderBook::addOrder(Side side, int price, U64 quantity, U64 timestamp) {
-    // create new order
-    Order order(orderId_, participantId_, side, price, quantity, timestamp);
+void OrderBook::addOrder(string instrumentName, Side side, int price, U64 quantity, U64 timestamp) {
+    // check if current instrument has entered book before
+    bool found;
+    U64 instrumentId;
+    if (instrumentToId_.find(instrumentName) == instrumentToId_.end()) {
+        instrumentToId_[instrumentName] = instrumentId_++;
+        found = false;
+    }
+    else {
+        instrumentId = instrumentToId_[instrumentName];
+        found = true;
+    }
 
+    // create new order
+    
+    vector<U64> toRemove;
     // add to appropriate map and list
-    if (side == BUY) bids_[price].push_back(order);
-    else asks_[price].push_back(order);
+    // TODO: matching algo code is duplicated for now. Need to figure out modularity soon.
+    Order* order;
+    if (side == BUY) {
+        if (found) {
+            for (auto& [p, orderList] : asks_) {
+                if (p > price) break; // break outer loop if p becomes greater than max buy price
+                if (quantity == 0) break; // early return for order completion before insertion
+                
+                for (auto it = orderList.begin(); it != orderList.end(); it++) {
+                    if (it->instrumentId == instrumentId) {
+                        if (quantity >= it->quantity) {
+                            quantity -= it->quantity;
+                            
+                            // just 'cancel' sell order for now (it is complete)
+                            // will create seperate function for completed orders later
+                            toRemove.push_back(it->orderId);
+                        }
+                        else {
+                            it->quantity -= quantity;
+                            quantity = 0;
+                        }
+                    } 
+                }
+            }    
+        }
+        if (quantity > 0) {
+            order = new Order(orderId_, participantId_, instrumentId, side, price, quantity, timestamp);
+            bids_[price].push_back(*order);
+        }
+    }
+    
+    else {
+        if (found) {
+            for (auto& [p, orderList] : asks_) {
+                if (p < price) break; // break outer loop if p becomes lesser than minimum sell price
+                if (quantity == 0) break; // early break for order completion before insertion
+                
+                for (auto it = orderList.begin(); it != orderList.end(); it++) {
+                    if (it->instrumentId == instrumentId) {
+                        if (quantity >= it->quantity) {
+                            quantity -= it->quantity;
+                            
+                            // just 'cancel' sell order for now (it is complete)
+                            // will create seperate function for completed orders later
+                            toRemove.push_back(it->orderId);
+                        }
+                        else {
+                            it->quantity -= quantity;
+                            quantity = 0;
+                        }
+                    } 
+                }
+            }    
+        }
+        if (quantity > 0) {
+            order = new Order(orderId_, participantId_, instrumentId, side, price, quantity, timestamp);
+            asks_[price].push_back(*order);
+        }
+    }
+
+    if (quantity == 0) {
+        for (int const& orderId : toRemove) cancelOrder(orderId);
+        return;
+    }
     
     // add to hashmap for quick lookups for future
-    orderMap_[orderId_] = order;
+    orderMap_[orderId_] = *order;
     orderId_++; // increment order id for now. more sophistication later on
 }
 
--- src/orderbook.h
+++ src/orderbook.h
@@ -4,12 +4,14 @@
 #include <list>
 #include <map>
 #include <unordered_map>
+#include <string>
 using namespace std;
 
 class OrderBook {
 private:
     U64 orderId_;
     U64 participantId_;
+    U64 instrumentId_;
     // Bids: sorted descending by price  
     map<int, list<Order>, std::greater<double>> bids_;
     
@@ -17,10 +19,13 @@ private:
     map<int, list<Order>, std::less<double>> asks_;
 
     // Fast lookup: order_id → iterator into the list + side + price
-    unordered_map<uint64_t, Order> orderMap_;
+    unordered_map<U64, Order> orderMap_;
+
+    // Fast lookup: order_id → iterator into the list + side + price
+    unordered_map<string, U64> instrumentToId_;
 public:
     OrderBook();
-    void addOrder(Side side, int price, U64 quantity, U64 timestamp);
+    void addOrder(string instrumentName, Side side, int price, U64 quantity, U64 timestamp);
     void cancelOrder(U64 orderId);
     void updateOrder(U64 orderId, int newPrice, int newQuantity);
 };
