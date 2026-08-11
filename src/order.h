e23b54f33ce82a3dd33c93e16a35f107f2bbf3f0
priyansh3010 <pri.obhan30@gmail.com>
2026-08-11 22:38:51 +0530

    fix: added instrument id to correctly differentiate between financial instruments

--- src/order.h
+++ src/order.h
@@ -5,11 +5,14 @@ using namespace std;
 struct Order {
     U64 orderId;
     U64 participantId; // ignored for now. Will implement in the future once basic implementation is done
+    U64 instrumentId; // identifier for each financial instrument (stocks, bonds, crypto, etc.)
     Side side;
     int price; // in cents - 10050 = $100.50
     U64 quantity;
     U64 timestamp;
 
-    Order(U64 order_id, U64 participant_id, Side s, int p, U64 q, U64 t)
-    : orderId(order_id), participantId(participant_id), side(s), price(p), quantity(q), timestamp(t) {}
+    Order() = default;
+
+    Order(U64 order_id, U64 participant_id, U64 instrument_id, Side s, int p, U64 q, U64 t)
+    : orderId(order_id), participantId(participant_id), instrumentId(instrument_id), side(s), price(p), quantity(q), timestamp(t) {}
 };
