6aadf505d525a826e4228cad9c0786495fbe3f24
priyansh3010 <pri.obhan30@gmail.com>
2026-08-18 18:43:53 +0530

    feat: added getLSB and getMSB functions

--- src/types.h
+++ src/types.h
@@ -5,3 +5,15 @@ typedef uint64_t U64;
 typedef uint8_t U8;
 
 enum Side { BUY, SELL };
+
+inline int getLSB(U64 word) {
+    unsigned long index;
+    _BitScanForward64(&index, word);
+    return (int)index;
+}
+
+inline int getMSB(U64 word) {
+    unsigned long index;
+    _BitScanForward64(&index, word);
+    return (int)index;
+}
